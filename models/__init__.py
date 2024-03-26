b5cd92e1d6a4df7137da8066a238d03d1c1435bb
root <root@DESKTOP-HU468P5.localdomain>
2024-03-26 15:37:02 +0200

    pycodestyle

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
-#make storge object
+# make storge object
 storage = FileStorage()
 storage.reload()
